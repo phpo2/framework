428d1ceb30f19c953257129ab9b73617b029a318
Nadeen Gamage <nadeengamage@Nadeens-MacBook-Pro.local>
2018-02-09 12:31:15 +0530

    bootstrap the phpo2 framework

---
+++ PHPO2/Application.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace PHPO2;
+
+/**
+* 
+* Create the Application.
+*
+* Here we will load the environment and create the application instance
+* that serves as the central piece of this framework.
+*/
+class Application
+{
+
+}
