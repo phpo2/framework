a508e8dd441236d95d90ed950f8fbf03cdc3943d
Nadeen Gamage <nadeengamage@Nadeens-MacBook-Pro.local>
2018-02-09 13:04:39 +0530

    register phpo2 exception handler and repoter

--- PHPO2/Application.php
+++ PHPO2/Application.php
@@ -2,6 +2,8 @@
 
 namespace PHPO2;
 
+use PHPO2\Exception\PHPO2Exception;
+
 /**
 * 
 * Create the Application.
@@ -11,5 +13,52 @@ namespace PHPO2;
 */
 class Application
 {
+	/**
+     * Run application.
+     *
+     * This method traverses the application middleware stack and then sends the
+     * resultant Response object to the HTTP client.
+     *
+     * @throws Exception
+     */
+	public function run()
+	{
+		$this->exception();
+	}
 
+	/**
+	 *
+	 * Load the Exception handler. Register framework exception handler and error reporter.
+	 *
+	 * @return void
+	 */
+	public function exception()
+	{
+		$this->enableErrorReporting();
+
+		$this->registerHandler();
+	}
+
+	/**
+	 *
+	 * Register PHPO2 framework exception handler.
+	 *
+	 * @return void
+	 */
+	public function registerHandler()
+	{
+		set_error_handler('PHPO2\Exception\PHPO2Exception::errorHandler');
+		set_exception_handler('PHPO2\Exception\PHPO2Exception::exceptionHandler');
+	}
+
+	/**
+	 *
+	 * Enable all error reporting.
+	 *
+	 * @return void
+	 */
+	public function enableErrorReporting()
+	{
+		error_reporting(E_ALL);
+	}
 }
