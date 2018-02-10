b45ae274860682279d4be95c0856a91cde9d1e66
Nadeen Gamage <nadeengamage@Nadeens-MacBook-Pro.local>
2018-02-10 15:04:08 +0530

    router exception class

---
+++ PHPO2/Exception/RouteException.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace PHPO2\Exception;
+
+use PHPO2\Exception\PHPO2Exception;
+
+/**
+* 
+* RouteException Exception.
+*
+* This Exception is thrown when the application 
+* could not find the any particular routes.
+*
+*/
+class RouteException extends PHPO2Exception
+{
+	
+}
