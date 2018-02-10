0b60d09ca878addabaaea435fd40fed88f53e838
Nadeen Gamage <nadeengamage@Nadeens-MacBook-Pro.local>
2018-02-10 15:01:52 +0530

    exception handler class improved v1.0.0

--- PHPO2/Exception/PHPO2Exception.php
+++ PHPO2/Exception/PHPO2Exception.php
@@ -25,10 +25,26 @@ class PHPO2Exception extends Exception
      *
      * @return void
      */
-    public static function errorHandler($level, $message, $file, $line)
+    public static function errorHandler()
     {
-        if (error_reporting() !== 0) {  // to keep the @ operator working
-            throw new ErrorException($message, 0, $level, $file, $line);
+        if (error_reporting() !== 0) {
+
+            ini_set('display_errors', 'Off');
+
+            $error = debug_backtrace();
+
+            unset($error[0]['args'][0]);
+            
+            self::responseCode(200);
+
+            self::renderException(
+                'fatal', 
+                $error[0]['file'], 
+                $error[0]['args'][1], 
+                array_filter($error[0]['args']), 
+                'ErrorException', $error[0]['line'], 
+                500
+            );
         }
     }
 
@@ -41,7 +57,45 @@ class PHPO2Exception extends Exception
      */
     public static function exceptionHandler($exception)
     {
-    	self::render($exception);
+        self::responseCode($exception->getCode());
+
+        self::renderException(
+            null,
+            get_class($exception), 
+            $exception->getMessage(), 
+            explode('#',$exception->getTraceAsString()), 
+            $exception->getFile(), 
+            $exception->getLine(), 
+            $exception->getCode()
+        );
+    }
+
+    /**
+     *
+     * Register and render all exception and error handler.
+     *
+     * @param Exception string $type
+     * @param Exception string $class
+     * @param Exception string $message
+     * @param Exception array $trace
+     * @param Exception string $throw
+     * @param Exception integer $line
+     * @param Exception integer $code
+     * @return void
+     */
+    public static function renderException($type = null, $class, $message, $trace, $throw, $line, $code)
+    {
+        $exception = [
+            'type'    => $type,
+            'class'   => $class,
+            'message' => $message,
+            'trace'   => $trace,
+            'throw'   => $throw,
+            'line'    => $line,
+            'code'    => $code
+        ];
+
+        self::render($exception);
     }
 
     /**
@@ -53,18 +107,10 @@ class PHPO2Exception extends Exception
      */
     public static function render($exception)
     {
-    	$exception = [
-    		'class' => get_class($exception),
-    		'message' => $exception->getMessage(),
-    		'trace' => explode('#',$exception->getTraceAsString()),
-    		'throw' => $exception->getFile(),
-    		'line' => $exception->getLine()
-    	];
-
-    	if (isset($exception)) {
-    		extract($exception);
-    		include 'Resource/error-render-page.php';	
-    	}
+        if (isset($exception)) {
+            extract($exception);
+            include 'Resource/error-render-page.php';   
+        }
     }
 
     /**
@@ -87,4 +133,17 @@ class PHPO2Exception extends Exception
 
     	error_log($message);
     }
+
+    /**
+     *
+     * Response HTTP status code.
+     *
+     * @param integer $code
+     * @return void
+     */
+    public static function responseCode($code)
+    {
+        http_response_code($code);
+    }
+   
 }
