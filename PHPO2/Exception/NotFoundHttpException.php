85c673b09c84e14781f338bdd976bfde44324503
Nadeen Gamage <nadeengamage@Nadeens-MacBook-Pro.local>
2018-02-10 15:02:58 +0530

    http not found exception class

---
+++ PHPO2/Exception/NotFoundHttpException.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace PHPO2\Exception;
+
+use PHPO2\Exception\PHPO2Exception;
+
+/**
+* 
+* NotFoundHttpException Exception.
+*
+* This Exception is thrown when the application
+* could not find HTTP Verbs the any particular rouets.
+*
+*/
+class NotFoundHttpException extends PHPO2Exception
+{
+	
+}
