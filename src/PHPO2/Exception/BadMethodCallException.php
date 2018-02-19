e6496d81f81381194a0c56d803746b7bbfa08bd4
phpo2-components <nadeengamage@Nadeens-MacBook-Pro.local>
2018-02-19 20:54:22 +0530

    throw bad method exception

---
+++ src/PHPO2/Exception/BadMethodCallException.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace PHPO2\Exception;
+
+use PHPO2\Exception\PHPO2Exception;
+
+/**
+* 
+* BadMethodCallException Exception.
+*
+* This Exception is thrown when the application 
+* could not find the any particular controller 
+* and method.
+*
+*/
+class BadMethodCallException extends PHPO2Exception
+{
+	
+}
